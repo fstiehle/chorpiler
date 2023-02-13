bfc324a02fe450803063b248d3f2969cb3d72f8b
Fabian <f.stiehle@gmail.com>
2023-02-13 18:20:07 +0100

    version

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import SolidityEnactment from './Generator/target/Sol/Enactment';
 import { SolidityStateChannelRoot } from './Generator/target/Sol/StateChannelRoot';
+import TypeScriptEnactment from './Generator/target/Typescript/Enactment';
 import { INetFastXMLParser } from './Parser/Parser';
 
 export default {
   Parser: INetFastXMLParser,
   Generator: {
     Sol: {
-      ProcessContract: SolidityEnactment,
+      Enactment: SolidityEnactment,
       StateChannelRoot: SolidityStateChannelRoot
+    },
+    TS: {
+      Enactment: TypeScriptEnactment
     }
   }
 }
