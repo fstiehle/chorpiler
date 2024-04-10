df0b9db137f9e4ff3b3bac2637f0044f5d063f34
Fabian <f.stiehle@gmail.com>
2024-04-10 19:23:41 +0200

    impl testing of case

--- tests/output/execution.test.ts
+++ tests/output/execution.test.ts
@@ -3,12 +3,12 @@
  */
 import { expect, use } from "chai";
 import chaiAsPromised from 'chai-as-promised';
-import util from 'util';
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import path from "path";
 import { BPMN_PATH } from "../config";
 import { XESFastXMLParser } from "../../src/util/ParseXES";
-import { EventLog, Trace } from "../../src/util/EventLog";
+import { EventLog } from "../../src/util/EventLog";
+import { Wallet, Contract, ContractFactory } from 'ethers';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
 import { ProcessEncoding } from "../../src/Generator/ProcessEncoding";
 
@@ -19,71 +19,103 @@ import { SC_ProcessEnactment } from './../data/generated/artifcats/types/SC_Proc
 
 import encodingSC from './../data/generated/supply-chain/SC_ProcessExecution_encoding.json';
 import assert from "assert";
-
-const readFile = util.promisify(fs.readFile);
-use(chaiAsPromised);
 use(solidity);
 
 const parser = new XESFastXMLParser();
 
 describe('Test Execution of Cases', () => {
 
-  describe('Supply Chain Case', () => {
-    let eventLog: EventLog;
+  describe('Supply Chain Case', async () => {
+    const eventLog = await parser.fromXML(
+      readFileSync(path.join(BPMN_PATH, 'cases', 'supply-chain', 'supply-chain.xes')));
+
     const processEncoding = ProcessEncoding.fromJSON(encodingSC);
-    const participants = new Map<String, SC_ProcessEnactment>;
-    let contract: SC_ProcessEnactment;
-    let totalGas = 0;
+    const factory = new ContractFactory(ASC_ProcessSmartContract.abi, ASC_ProcessSmartContract.bytecode);
+    
+    describe(`Replay Conforming Traces`, () => {
 
-    before(async () => {
-      const data = await readFile(path.join(BPMN_PATH, 'cases', 'supply-chain', 'supply-chain.xes'));
-      eventLog = await parser.fromXML(data);
+      // Requires a foreach to work: https://github.com/mochajs/mocha/issues/3074
+      eventLog.traces.forEach((trace, i) => {
+
+        it(`Replay Conforming Trace ${i}`, async () => {
+          const r = await deploy(factory, processEncoding);
+          const contracts = r.contracts as Map<string, SC_ProcessEnactment>;
+          let totalGasCost = r.tx.gasUsed.toNumber();
+          console.log('Gas', 'Deployment', ':', totalGasCost);
+          const contract = [...contracts.values()][0];
+
+          for (const event of trace) {
+            const participant = contracts.get(event.source);
+            const taskID = processEncoding.tasks.get(event.name);
+            assert(participant !== undefined && taskID !== undefined,
+              `source '${event.source}' event '${event.name}' not found`);
+
+            const preTokenState = await contract.tokenState();
+            const tx = await (await participant.enact(taskID)).wait(1);
+            // Expect that tokenState has changed!
+            expect(await contract.tokenState()).to.not.equal(preTokenState);
+            // console.debug('Gas', 'Enact Task', event.name, ":", tx.gasUsed.toNumber());
+            totalGasCost += tx.gasUsed.toNumber();
+          }
+          expect(
+            await contract.tokenState(), 
+            "End of process not reached!"
+          ).to.equal(0);
+          console.log('Gas', 'Total', ':', totalGasCost);
+        })
+      })
     })
 
-    beforeEach(async () => {
-      const wallets = new MockProvider().getWallets().slice(0, processEncoding.participants.size);
+    describe(`Replay Non-Conforming Traces`, () => {
 
-      contract = (await deployContract(
-        wallets[0], 
-        ASC_ProcessSmartContract, 
-        [[...[...wallets.values()].map(v => v.address)]])
-        ) as SC_ProcessEnactment;
+      const badLog = EventLog.genNonConformingLog(eventLog, processEncoding, 2);
+      
+      // Requires a foreach to work: https://github.com/mochajs/mocha/issues/3074
+      badLog.traces.forEach((trace, i) => {
 
-        let tx = await contract.deployTransaction.wait(1);
-        let cost = tx.gasUsed.toNumber();
-        console.log('Gas', 'Deployment:', cost);
-        totalGas += cost;
+        it(`Replay Non-Conforming Trace ${i}`, async () => {
+          const r = await deploy(factory, processEncoding);
+          const contracts = r.contracts as Map<string, SC_ProcessEnactment>;
+          const contract = [...contracts.values()][0];
 
-        for (const [id, num] of processEncoding.participants) {
-          participants.set(id, contract.connect(wallets[num]));
-        }
-    });
+          let eventsRejected = 0;
+          for (const event of trace) {
 
-    it('Replay Conforming Traces', async () => {
-      for (const trace of eventLog.traces) {
-        await replayTrace(trace, participants, processEncoding);
-        expect(
-          contract.tokenState(), 
-          "End of process not reached!"
-        ).to.eventually.equal(0);
-      }
+            const participant = contracts.get(event.source);
+            const taskID = processEncoding.tasks.get(event.name);
+            assert(participant !== undefined && taskID !== undefined,
+              `source '${event.source}' event '${event.name}' not found`);
+            
+            const preTokenState = await contract.tokenState();
+            await (await participant.enact(taskID)).wait(1);
+
+            if ((await contract.tokenState()).eq(preTokenState)) eventsRejected++;
+          }
+          
+          // Expect that tokenState has at least NOT changed once (one non-conforming event)
+          // or end event has not been reached (if only an event was removed, but no non-conforming was added)
+          expect(eventsRejected > 0 || !(await contract.tokenState()).eq(0));
+        })
+      })
     })
 
-    it('Replay Non-Conforming Traces', () => {
-      //console.log(EventLog.genNonConformingLog(eventLog, processEncoding, 60));
-    })
   })
 })
 
-const replayTrace = async (trace: Trace, 
-  contracts: Map<String, SC_ProcessEnactment>, 
-  processEncoding: ProcessEncoding) => {
-  for (const event of trace) {
-    const contract = contracts.get(event.source);
-    const taskID = processEncoding.tasks.get(event.name);
-    assert(contract !== undefined && taskID !== undefined,
-      `model log mismatch with source '${event.source}' event '${event.name}'`);
+const deploy = async (factory: ContractFactory, processEncoding: ProcessEncoding) => {
+  const wallets = new MockProvider()
+    .getWallets()
+    .slice(0, processEncoding.participants.size);
 
-    await contract.enact(taskID);
+  const contract = await factory
+    .connect(wallets[0])
+    .deploy([...[...wallets.values()].map(v => v.address)])
+  
+  const tx = await contract.deployTransaction.wait(1)
+
+  const contracts = new Map<string, Contract>();
+  for (const [id, num] of processEncoding.participants) {
+    contracts.set(id, contract.connect(wallets[num]));
   }
+  return {contracts, tx};
 }
